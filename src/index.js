3a2923b6ee6e5bab7a091a12eeed86472f43ddec
unknown <lapwailau@gmail.com>
2020-02-11 18:34:00 +1300

    add postReducer to get data from api

--- src/index.js
+++ src/index.js
@@ -1,37 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-
+import {
+  createStore,
+  combineRcombineReducers,
+  combineReducers,
+  compose,
+  applyMiddleware
+} from "redux";
+import thunk from "redux-thunk";
+import { get } from "axios";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-//action, reducer, store(state)
-
-const counterReducer = function(state = { count: 1 }, action) {
+// 1. create reducer and name it counterReducer and postReducer separately
+// 1.1 two arguments are in the reducer, "state" and "action"
+// counterReducer aims to count the numbers
+const counterReducer = function(state = { count: 7 }, action) {
   switch (action.type) {
     case "COUNT_ADD":
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + 7 };
+    case "COUNT_REDUCE":
+      return { ...state, count: state.count - 6 };
     default:
       return state;
   }
 };
 
-const store = createStore(
-  counterReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-console.log(store.getState());
+//postReducer aims to display data from API
+const postReducer = function(
+  state = { posts: [{ title: "Post Title" }] },
+  action
+) {
+  switch (action.type) {
+    case "GET_POSTS":
+      return { ...state, posts: action.payload };
+    default:
+      return state;
+  }
+};
 
-store.dispatch({
-  type: "COUNT_ADD",
-  payload: {}
+//1.2 using "combineReducers" to join two reducers together
+const rootReducers = combineReducers({
+  couter: counterReducer,
+  post: postReducer
 });
-console.log(store.getState());
+
+// 2. create store by "combineReducers"
+const store = createStore(
+  rootReducers,
+  compose(
+    applyMiddleware(...[thunk]),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+//getPostRequest()
+const getPostRequest = () => {
+  //return json promise object
+  return get("https://jsonplaceholder.typicode.com/posts");
+};
+
+store.dispatch(async function(dispatch) {
+  const response = await getPostRequest();
+  // console.log(response.data);
+  dispatch({
+    type: "GET_POSTS",
+    payload: response.data
+  });
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
